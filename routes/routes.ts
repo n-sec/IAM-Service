06eb0d9fd829d2f4d23e1752af55f6d8aa3182a5
Gerson Víctor <gerson.vfs@gmail.com>
2019-06-19 16:33:18 -0300

    Moved register route from routes.ts to register.ts

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,23 +1,7 @@
 import express from 'express';
-import { User } from '../models';
+
 
 export default (app: express.Express) => {
   
-  app.post('/credentials/register', (req, res) => {
-    User.create({
-      user: req.body.user,
-      pass: req.body.pass,
-    }).then((data) => {
-      res.send(data);
-    }).catch((error) => {
-      switch (error.code) {
-        case 11000:
-          res.sendStatus(409);
-          break;
-        default:
-          res.sendStatus(500);
-          break;
-      }
-    });
-  });
+
 }
