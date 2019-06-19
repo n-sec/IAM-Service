35df604ea3be0365144dd4354d4f4b83f339cb22
Gerson Víctor <gerson.vfs@gmail.com>
2019-06-19 16:29:26 -0300

    Moved authenticate route from routes.ts to authenticate.ts

--- routes/authenticate.ts
+++ routes/authenticate.ts
@@ -1,5 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../models';
 
 export default (app: express.Express) => {
-  
+  app.post('/credentials/authenticate', (req, res) => {
+    User.findOne({
+      user: req.body.user,
+      pass: req.body.pass,
+    }).then((data) => {
+      if (data) {
+        let token = jwt.sign(data.toObject(), 'abc@123');
+        res.send({token: token});
+      } else {
+        res.sendStatus(401);
+      }
+    }).catch((error) => {
+      res.sendStatus(500);
+    });
+  });
 }
--- routes/routes.ts
+++ routes/routes.ts
@@ -1,23 +1,7 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { User } from '../models';
 
 export default (app: express.Express) => {
-  app.post('/credentials/authenticate', (req, res) => {
-    User.findOne({
-      user: req.body.user,
-      pass: req.body.pass,
-    }).then((data) => {
-      if (data) {
-        let token = jwt.sign(data.toObject(), 'abc@123');
-        res.send({token: token});
-      } else {
-        res.sendStatus(401);
-      }
-    }).catch((error) => {
-      res.sendStatus(500);
-    });
-  });
   
   app.post('/credentials/register', (req, res) => {
     User.create({
